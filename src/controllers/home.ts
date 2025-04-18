3252e74c0007a05dca5b68b75a1f8e0bd9d4c459
DiiasDev <devpdias@gmail.com>
2025-04-18 20:35:34 -0300

    Adição de formulario para preencher item

---
+++ src/controllers/home.ts
@@ -0,0 +1,11 @@
+const cadastrarItem = (nome: string, descricao: string, quantidade: number, valor: number, img: string) => {
+
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const formItem = document.getElementById('formItem') as HTMLFormElement;
+
+
+
+});
